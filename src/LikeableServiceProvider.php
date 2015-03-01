6b60b941ac8da80b1cd7bd6668a48e248c568e8f
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-01 18:24:15 +0700

    Add Service Provider

---
+++ src/LikeableServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Namest\Likeable;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class LikeableServiceProvider
+ *
+ * @author  Nam Hoang Luu <nam@mbearvn.com>
+ * @package Namest\Likeable
+ *
+ */
+class LikeableServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
+}
