05e48d5f9f2a92ea57c5bd95979caf15475af75e
Luu Hoang Nam <hoangnam0705@icloud.com>
2015-03-01 19:21:33 +0700

    Register migrations

--- src/LikeableServiceProvider.php
+++ src/LikeableServiceProvider.php
@@ -13,6 +13,21 @@ use Illuminate\Support\ServiceProvider;
  */
 class LikeableServiceProvider extends ServiceProvider
 {
+    /**
+     * Boot up resources
+     */
+    public function boot()
+    {
+        // Publish a config file
+        $this->publishes([
+            __DIR__ . '/../config/likeable.php' => config_path('likeable.php')
+        ], 'config');
+
+        // Publish your migrations
+        $this->publishes([
+            __DIR__ . '/../database/migrations/' => base_path('/database/migrations')
+        ], 'migrations');
+    }
 
     /**
      * Register the service provider.
